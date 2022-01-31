56ae18c763dd6a729762ad6dc3bf54d821b438ba
Sumit Harijan <smtharijan@gmail.com>
2022-01-31 16:04:01 +0530

    final commit

--- Responsive sticky navbar/script.js
+++ Responsive sticky navbar/script.js
@@ -18,25 +18,4 @@ navLinks.forEach((link) => {
   });
 });
 
-document.addEventListener("contextmenu", (event) => event.preventDefault());
-document.onkeydown = function (e) {
-  // disable F12 key
-  if (e.keyCode == 123) {
-    return false;
-  }
 
-  // disable I key
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-    return false;
-  }
-
-  // disable J key
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-    return false;
-  }
-
-  // disable U key
-  if (e.ctrlKey && e.keyCode == 85) {
-    return false;
-  }
-};
